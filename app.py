50c08005072ad35a20c98c9c6bfa1637d22bd31c
aayush-kumar-codes <aayush.excel2011@gmail.com>
2025-04-15 12:37:25 +0530

    first commit on main

---
+++ app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Load Excel
+df = pd.read_excel('/home/python/Desktop/sudir code/Ayush_Regex.xlsx')
+
+# DB config
+engine = create_engine('postgresql+psycopg2://postgres:Java_123@localhost:5432/sukdeep')
+
+# Write to DB
+df.to_sql('ayush_regex', engine, if_exists='replace', index=False)
+
+print("Data inserted successfully!")
